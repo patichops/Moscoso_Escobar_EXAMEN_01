8935e8284b449479f36d887d2a662080c6647481
Patricio Moscoso <patrixiolel@gmail.com>
2023-05-25 18:33:21 -0500

    MOSCOSO ESCOBAR: 18:33

--- app/src/main/java/com/example/moscoso_escobar_examen_01/MainActivity.java
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/MainActivity.java
@@ -40,17 +40,20 @@ public class MainActivity extends AppCompatActivity {
         int valor = 0;
 
         for (int i = dato1.length() - 1; i == 0; i--) {
-            valor = Integer.parseInt(dato2.substring(i,1)) + Integer.parseInt(dato1.substring(i,1)) + sobrante;
+            valor = Integer.parseInt(dato2.substring(i)) + Integer.parseInt(dato1.substring(i)) + sobrante;
 
             if (valor > 10){
                 valor = valor - 10;
                 sobrante = 1;
             }
 
-            resultado = valor + resultado;
+            if (sobrante == 1)
+                resultado = String.valueOf(sobrante) + String.valueOf(valor) + resultado;
+            else
+                resultado = String.valueOf(valor) + resultado;
         }
 
-        return "";
+        return resultado;
     }
 
     public void onClick_SumarNumero(View view){
@@ -58,27 +61,26 @@ public class MainActivity extends AppCompatActivity {
         String dato2 = numero2.getText().toString(); //12
         String resultado = numero2.getText().toString();
 
-        if (dato1.length() > dato2.length()){
 
-            for (int i = 0; i < dato1.length() - dato2.length(); i++) {
-                dato2 = "0" + dato2; //0012
+
+        if (dato2.length() != dato1.length()) {
+            if (dato1.length() > dato2.length()){
+
+                for (int i = 0; i < dato1.length() - dato2.length(); i++) {
+                    dato2 = "0" + dato2; //0012
+                }
+                //pruebas.setText(dato1.charAt(0));
+                //pruebas.setText(dato1.substring(0,1));
             }
 
-            //pruebas.setText(dato1.charAt(0));
-            pruebas.setText(dato1.substring(0,1));
-
-        }
-
-        if (dato2.length() > dato1.length()) {
-
-            for (int i = 0; i < dato2.length() - dato1.length(); i++) {
-                dato1 = "0" + dato1; //0012
+            if (dato2.length() > dato1.length()) {
+                for (int i = 0; i < dato2.length() - dato1.length(); i++) {
+                    dato1 = "0" + dato1; //0012
+                }
             }
-
-
-
         }
 
-
+        String resp = sumar(dato1, dato2);
+        pruebas.setText(resp);
     }
 }

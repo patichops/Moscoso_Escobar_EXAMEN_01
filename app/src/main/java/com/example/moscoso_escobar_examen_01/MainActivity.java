edd42f66cf6f23bac9ebdd55beec34b94f569251
Patricio Moscoso <patrixiolel@gmail.com>
2023-05-25 17:34:11 -0500

    MOSCOSO ESCOBAR: 17:34

--- app/src/main/java/com/example/moscoso_escobar_examen_01/MainActivity.java
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.moscoso_escobar_examen_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText numero1;
+    private EditText numero2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //numero1 = 
+
+        String numero1 = "";
+        String numero2 = "";
+        String resultado = "";
+
+        short acarreo = 0;
+
+
+
     }
 }

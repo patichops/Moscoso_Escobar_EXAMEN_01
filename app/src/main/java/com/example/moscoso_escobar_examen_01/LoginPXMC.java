f8b8513435ab66be15d715ba1c0477b6e75fdd5d
Patricio Moscoso <patrixiolel@gmail.com>
2023-06-09 17:05:35 -0500

    Patricio Moscoso: 17:05

--- app/src/main/java/com/example/moscoso_escobar_examen_01/LoginPXMC.java
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/LoginPXMC.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.moscoso_escobar_examen_01.UsuarioPXMC;
 
 public class LoginPXMC extends AppCompatActivity {
 
@@ -19,7 +24,8 @@ public class LoginPXMC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
+
+
 
         final String USUARIOS="CREATE TABLE USUARIOS(nombre TEXT, clave TEXT)";
 
@@ -31,19 +37,39 @@ public class LoginPXMC extends AppCompatActivity {
 
         sql.insert("PRUEBADB","usuarios", values);
 
+        values = new ContentValues();
+        values.put("usuario", "usuario2");
+        values.put("clave", "1234");
+
+        sql.insert("PRUEBADB","usuarios", values);
+
         usuario = findViewById(R.id.editTextUsuarioPXMC);
         contraseña = findViewById(R.id.editTextContraseñaPXMC);
-
-
 
     }
 
     public void Onclick_Aceptar(View v){
 
+        if (usuario.getText().toString().isEmpty() || contraseña.getText().toString().isEmpty()) {
+            Toast.makeText(this, "LLene todos los campos", Toast.LENGTH_SHORT).show();
+        } else {
+            String user = usuario.getText().toString();
+            String passwd = contraseña.getText().toString();
+
+            String SELECT = "SELECT usuario, clave FROM USUARIOS " +
+                            "WHERE usuario = " + user +
+                            " AND clave = " + passwd;
+
+            Cursor cursor = sql.rawQuery(SELECT, null);
+
+            if (cursor.moveToFirst()){
+
+            } else {
+                Toast.makeText(this, "LLene todos los campos", Toast.LENGTH_SHORT).show();
+            }
+
+        }
 
 
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/UsuarioPXMC.java
@@ -0,0 +1,24 @@
+package com.example.moscoso_escobar_examen_01;
+
+public class UsuarioPXMC {
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    private String usuario;
+    private String contraseña;
+
+
+}

dd858cf32b14537ed004a3e2b573045db4a54a79
Patricio Moscoso <patrixiolel@gmail.com>
2023-06-09 16:31:16 -0500

    Patricio Moscoso: 19:30

---
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/LoginPXMC.java
@@ -0,0 +1,14 @@
+package com.example.moscoso_escobar_examen_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPXMC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

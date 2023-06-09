ce7e97d9e150b1aa30f96ebb36c1f7d81959d751
Patricio Moscoso <patrixiolel@gmail.com>
2023-06-09 16:50:04 -0500

    Patricio Moscoso: 19:50

---
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/DataBaseAdminPXMC.java
@@ -0,0 +1,13 @@
+package com.example.moscoso_escobar_examen_01;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DataBaseAdminPXMC {
+
+    private SQLiteDatabase sql;
+
+    public DataBaseAdminPXMC(Context context){
+
+    }
+}
--- app/src/main/java/com/example/moscoso_escobar_examen_01/LoginPXMC.java
+++ app/src/main/java/com/example/moscoso_escobar_examen_01/LoginPXMC.java
@@ -2,13 +2,48 @@ package com.example.moscoso_escobar_examen_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginPXMC extends AppCompatActivity {
+
+    private SQLiteDatabase sql;
+    private EditText usuario;
+    private EditText contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+
+        final String USUARIOS="CREATE TABLE USUARIOS(nombre TEXT, clave TEXT)";
+
+        sql.execSQL(USUARIOS);
+
+        ContentValues values = new ContentValues();
+        values.put("usuario", "usuario1");
+        values.put("clave", "1234");
+
+        sql.insert("PRUEBADB","usuarios", values);
+
+        usuario = findViewById(R.id.editTextUsuarioPXMC);
+        contraseña = findViewById(R.id.editTextContraseñaPXMC);
+
+
+
+    }
+
+    public void Onclick_Aceptar(View v){
+
+
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
